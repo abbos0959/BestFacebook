aac0a4e085c5896d1d2eb31083e6c5dce8b342b8
abbos0959 <abbosakromovich777@gmail.com>
2022-09-02 11:44:51 -0700

    redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
-function App() {
-   const get = async () => {
-      const res = await fetch("http://localhost:8000");
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/home";
 
-      console.log(res);
-   };
-   get();
-   return <div>welcome to frontend</div>;
+import { Login } from "./pages/login/index";
+import { Profile } from "./pages/profile/index";
+function App() {
+   return (
+      <div>
+         <Routes>
+            <Route path="/login" element={<Login />} exact />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/home" element={<Home/>}/>
+            
+         </Routes>
+      </div>
+   );
 }
 
 export default App;
